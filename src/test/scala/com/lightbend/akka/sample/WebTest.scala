b95433aee5f64e5d61c9469b8bf5900f3135847e
Dave Welch <david@davidwelch.co>
2018-02-21 10:04:11 -0700

    cleanup

--- src/test/scala/com/lightbend/akka/sample/WebTest.scala
+++ src/test/scala/com/lightbend/akka/sample/WebTest.scala
@@ -1,17 +1,12 @@
 package com.lightbend.akka.sample
 
 import akka.http.scaladsl.testkit.ScalatestRouteTest
-import com.lightbend.akka.sample.domain.customers.{Customer, CustomerRepo, CustomerService, Customers, CustomerJsonSupport}
+import com.lightbend.akka.sample.domain.customers._
 import com.lightbend.akka.sample.routes.CustomersRoute
-import org.scalamock.proxy.ProxyMockFactory
-import org.scalamock.scalatest.{AbstractMockFactory, MixedMockFactory, MockFactory}
+import org.scalamock.scalatest.MixedMockFactory
 import org.scalatest._
 
-import scala.concurrent.duration._
-import scala.concurrent.{Await, ExecutionContext, Future}
-import scala.reflect.ClassTag
-import akka.http.scaladsl.server._
-import Directives._
+import scala.concurrent.{ExecutionContext, Future}
 
 class WebTest
     extends WordSpec
@@ -22,12 +17,13 @@ class WebTest
 
   implicit val ec = ExecutionContext.global
 
-  val expected = new Customers( List[Customer](
-    new Customer(None, "jimbo@foobar.com"),
-    new Customer(None, "franky@foobar.com"),
-    new Customer(None, "foo@bar.com"),
-    new Customer(None, "alice@FooBar.com")
-  ))
+  val expected = new Customers(
+    List[Customer](
+      new Customer(None, "jimbo@foobar.com"),
+      new Customer(None, "franky@foobar.com"),
+      new Customer(None, "foo@bar.com"),
+      new Customer(None, "alice@FooBar.com")
+    ))
   val repo = mock[CustomerRepo]
   (repo.stream _)
     .expects()
@@ -43,18 +39,10 @@ class WebTest
       Get("/customers/") ~> route ~> check {
 //        responseAs[String] shouldEqual "Captain on the bridge!"
 //        entityAs[String] should === ("blah")
-        entityAs[Customers] should === (expected)
+        entityAs[Customers] should ===(expected)
       }
 
     }
-
-//    "dummy2" in {
-//      val repo = mock[CustomerRepo]
-//
-//      val svc = new CustomerService(repo)
-//      val matches = Await.result(svc.getByDomain("foobar.com"), 2 seconds)
-//      assert(matches.size == 3, "Count of matches was wrong")
-//    }
   }
 
 }
