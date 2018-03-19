d3e7601cad7b0a0ac9fccc0a68e438bf7f217747
Dave Welch <david@davidwelch.co>
2018-03-19 14:33:43 -0600

    test

--- src/test/scala/com/lightbend/akka/sample/ThreadLocalTest.scala
+++ src/test/scala/com/lightbend/akka/sample/ThreadLocalTest.scala
@@ -1,8 +1,12 @@
 package com.lightbend.akka.sample
 
+import java.util.concurrent.{Executors, ThreadPoolExecutor, TimeUnit}
+
 import com.lightbend.akka.sample.domain.customers.{Customer, CustomerRepo, CustomerService, Customers}
 import org.scalamock.scalatest.{MixedMockFactory, MockFactory}
 import org.scalatest._
+
+import scala.concurrent.ExecutionContextExecutor
 
 //import scala.concurrent.duration._
 import scala.concurrent.{Await, ExecutionContext, Future}
@@ -18,49 +22,74 @@ class ThreadLocalTest extends WordSpec with MockFactory with MixedMockFactory {
     "can Stuff" should {
 
       "dummy" in {
-        implicit val ec = scala.concurrent.ExecutionContext.fromExecutorService(
-          new ForkJoinPoolWithDynamicVariable
+
+
+
+
+//        val delegate = scala.concurrent.ExecutionContext.global
+        val tp = Executors.newFixedThreadPool(5)
+        val delegate = scala.concurrent.ExecutionContext.fromExecutorService(
+          tp
         )
 
-        ForkJoinPoolWithDynamicVariable.set("blah")
+        implicit val ec = new SmartExecutorService(delegate)
+        LocalContext.set("blah")
 
-        val finalResult = Await.result(Future[Any] {
-          val something = ForkJoinPoolWithDynamicVariable.get()
-          println(s"Got something ${something}")
+        val worker = () => Future[Any] {
+          Thread.sleep(2 * 1000)
+          val something = LocalContext.get()
+          println(s"Got something ${something} in thread ${Thread.currentThread().getId}")
           something
-        }, 3.seconds)
+        }
 
-        println(s"Got final result ${finalResult}")
+        Await.result(worker(), 3.seconds)
+        Await.result(worker(), 3.seconds)
+        Await.result(worker(), 3.seconds)
+
+        LocalContext.set("blah2")
+        Await.result(worker(), 3.seconds)
+        Await.result(worker(), 3.seconds)
+        Await.result(worker(), 3.seconds)
+
+//        tp.awaitTermination(1, TimeUnit.MINUTES)
+
+        println(s"Got final result")
       }
     }
   }
-
-
 }
 
-
-import scala.concurrent.forkjoin._
-
-class ForkJoinPoolWithDynamicVariable
-  extends ForkJoinPool {
+class SmartExecutorService(delegate: ExecutionContextExecutor) extends ExecutionContextExecutor {
+  override def reportFailure(cause: Throwable): Unit = {
+    println(cause)
+  }
 
   override def execute(task: Runnable) {
 
-    val value = ForkJoinPoolWithDynamicVariable.get()
-    super.execute(new Runnable {
+    val value = LocalContext.get()
+    delegate.execute(new Runnable {
       override def run = {
-        ForkJoinPoolWithDynamicVariable.set(value)
+//        LocalContext.set(value)
         task.run
       }
     })
   }
 }
 
-object ForkJoinPoolWithDynamicVariable {
-  private val store = new DynamicVariable[Any]
-  def set(value: Any) = {
-    store.value = value
-  }
 
-  def get() = store.value
+object LocalContext {
+
+  private var value:Any = null
+
+  def set(value:Any) = {
+    this.value = value
+  }
+  def get() = value
+
+//  private val store = new DynamicVariable[Any]
+//  def set(value: Any) = {
+//    store.value = value
+//  }
+//
+//  def get() = store.value
 }
